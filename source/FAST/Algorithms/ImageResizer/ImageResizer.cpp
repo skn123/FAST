0c6b957ebe01952a3050e4a97ae6f9829abe6687
Erik Smistad <ersmistad@gmail.com>
2026-02-05 15:56:37 +0100

    Fix bug when one downsampling factor is less than 1, and the other is above 1 in gaussian smoothing

--- source/FAST/Algorithms/ImageResizer/ImageResizer.cpp
+++ source/FAST/Algorithms/ImageResizer/ImageResizer.cpp
@@ -112,7 +112,11 @@ void ImageResizer::execute() {
             std::vector<float> stddev;
             for(int i = 0; i < input->getDimensions(); ++i) {
                 float downsampleFactor = (float)input->getSize()[i]/(float)mSize[i];
-                stddev.push_back((downsampleFactor - 1.0f)/2.0f);
+                if(downsampleFactor <= 1.0f) {
+                    stddev.push_back(0.0f);
+                } else {
+                    stddev.push_back((downsampleFactor - 1.0f)/2.0f);
+                }
             }
             if(input->getDimensions() == 2) {
                 reportInfo() << "Pre smoothing image in ImageResizer with std dev " << stddev[0] << " " << stddev[1] << reportEnd();
