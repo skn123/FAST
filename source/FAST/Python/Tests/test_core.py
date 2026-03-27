c6b478afe6451cd626c0f667653f2930ddbf93d0
Erik Smistad <ersmistad@gmail.com>
2026-03-27 15:15:56 +0100

    Simplify test_core.py

--- source/FAST/Python/Tests/test_core.py
+++ source/FAST/Python/Tests/test_core.py
@@ -4,39 +4,33 @@ import pytest
 
 def test_callable_process_object():
     image = fast.Image.create(256, 256, fast.TYPE_UINT8, 1)
-    image2 = fast.ImageResizer.create(512, 512)(image)
-    assert image2.getWidth() == 512
-    assert image2.getHeight() == 512
-
-    image2 = fast.ImageResizer.create(512, 512)((0, image))
-    assert image2.getWidth() == 512
-    assert image2.getHeight() == 512
-
-    resize = fast.ImageResizer.create(512, 512).connect(image)
-    image2 = fast.GrayscaleToColor.create()(resize)
-    assert image2.getWidth() == 512
-    assert image2.getHeight() == 512
+    image2 = fast.GrayscaleToColor.create()(image)
     assert image2.getNrOfChannels() == 3
 
-    image2 = fast.GrayscaleToColor.create()((0, resize, 0))
-    assert image2.getWidth() == 512
-    assert image2.getHeight() == 512
+    image2 = fast.GrayscaleToColor.create()((0, image))
     assert image2.getNrOfChannels() == 3
+
+    convert = fast.GrayscaleToColor.create().connect(image)
+    image2 = fast.ColorToGrayscale.create()(convert)
+    assert image2.getNrOfChannels() == 1
+
+    image2 = fast.ColorToGrayscale.create()((0, convert, 0))
+    assert image2.getNrOfChannels() == 1
 
 
 def test_callable_process_object_exceptions():
     with pytest.raises(ValueError):
-        image2 = fast.ImageResizer.create(512, 512)(32)
+        image2 = fast.GrayscaleToColor.create()(32)
     with pytest.raises(RuntimeError):
-        image2 = fast.ImageResizer.create(512, 512)()
+        image2 = fast.GrayscaleToColor.create()()
     image = fast.Image.create(256, 256, fast.TYPE_UINT8, 1)
     with pytest.raises(RuntimeError):
-        image2 = fast.ImageResizer.create(512, 512)(image, image)
+        image2 = fast.GrayscaleToColor.create()(image, image)
     with pytest.raises(RuntimeError):
-        image2 = fast.ImageResizer.create(512, 512)((1, image))
+        image2 = fast.GrayscaleToColor.create()((1, image))
 
-    resize = fast.ImageResizer.create(512, 512).connect(image)
+    convert = fast.GrayscaleToColor.create().connect(image)
     with pytest.raises(RuntimeError):
-        image2 = fast.GrayscaleToColor.create()((0, resize, 3))
+        image2 = fast.ColorToGrayscale.create()((0, convert, 3))
     with pytest.raises(ValueError):
-        image2 = fast.GrayscaleToColor.create()((0, resize, 3, 2))
+        image2 = fast.ColorToGrayscale.create()((0, convert, 3, 2))
