1236c6ae8546d28789c2259b0910c9a9e209ca5b
Erik Smistad <ersmistad@gmail.com>
2026-09-08 13:06:37 +0200

    Added python tests for new way of creating instance of shared_ptr fast objects

--- source/FAST/Python/Tests/test_core.py
+++ source/FAST/Python/Tests/test_core.py
@@ -3,34 +3,35 @@ import pytest
 
 
 def test_callable_process_object():
-    image = fast.Image.create(256, 256, fast.TYPE_UINT8, 1)
-    image2 = fast.GrayscaleToColor.create()(image)
+    image = fast.Image(256, 256, fast.TYPE_UINT8, 1)
+    image.fill(0)
+    image2 = fast.GrayscaleToColor()(image)
     assert image2.getNrOfChannels() == 3
 
-    image2 = fast.GrayscaleToColor.create()((0, image))
+    image2 = fast.GrayscaleToColor()((0, image))
     assert image2.getNrOfChannels() == 3
 
-    convert = fast.GrayscaleToColor.create().connect(image)
-    image2 = fast.ColorToGrayscale.create()(convert)
+    convert = fast.GrayscaleToColor().connect(image)
+    image2 = fast.ColorToGrayscale()(convert)
     assert image2.getNrOfChannels() == 1
 
-    image2 = fast.ColorToGrayscale.create()((0, convert, 0))
+    image2 = fast.ColorToGrayscale()((0, convert, 0))
     assert image2.getNrOfChannels() == 1
 
 
 def test_callable_process_object_exceptions():
     with pytest.raises(ValueError):
-        image2 = fast.GrayscaleToColor.create()(32)
+        image2 = fast.GrayscaleToColor()(32)
     with pytest.raises(RuntimeError):
-        image2 = fast.GrayscaleToColor.create()()
-    image = fast.Image.create(256, 256, fast.TYPE_UINT8, 1)
+        image2 = fast.GrayscaleToColor()()
+    image = fast.Image(256, 256, fast.TYPE_UINT8, 1)
     with pytest.raises(RuntimeError):
-        image2 = fast.GrayscaleToColor.create()(image, image)
+        image2 = fast.GrayscaleToColor()(image, image)
     with pytest.raises(RuntimeError):
-        image2 = fast.GrayscaleToColor.create()((1, image))
+        image2 = fast.GrayscaleToColor()((1, image))
 
-    convert = fast.GrayscaleToColor.create().connect(image)
+    convert = fast.GrayscaleToColor().connect(image)
     with pytest.raises(RuntimeError):
-        image2 = fast.ColorToGrayscale.create()((0, convert, 3))
+        image2 = fast.ColorToGrayscale()((0, convert, 3))
     with pytest.raises(ValueError):
-        image2 = fast.ColorToGrayscale.create()((0, convert, 3, 2))
+        image2 = fast.ColorToGrayscale()((0, convert, 3, 2))
--- source/FAST/Python/Tests/test_shortcuts.py
+++ source/FAST/Python/Tests/test_shortcuts.py
@@ -4,8 +4,8 @@ import pytest
 timeout = 1000 # Set to None for debugging
 
 def test_display2D():
-    importer = fast.ImageFileImporter.create(fast.Config.getTestDataPath() + 'US/JugularVein/US-2D_100.mhd')
-    segmentation = fast.BinaryThresholding.create(100).connect(importer)
+    importer = fast.ImageFileImporter(fast.Config.getTestDataPath() + 'US/JugularVein/US-2D_100.mhd')
+    segmentation = fast.BinaryThresholding(100).connect(importer)
 
     fast.display2D(importer, segmentation, timeout=timeout, bgcolor=fast.Color.Black())
 
@@ -23,8 +23,8 @@ def test_display2D():
 
 
 def test_display2D_render_to_image():
-    importer = fast.ImageFileImporter.create(fast.Config.getTestDataPath() + 'US/JugularVein/US-2D_100.mhd')
-    segmentation = fast.BinaryThresholding.create(100).connect(importer)
+    importer = fast.ImageFileImporter(fast.Config.getTestDataPath() + 'US/JugularVein/US-2D_100.mhd')
+    segmentation = fast.BinaryThresholding(100).connect(importer)
 
     image = fast.display2D(
         importer,
@@ -44,11 +44,11 @@ def test_display2D_render_to_image():
 
 
 def test_display2D_return_window():
-    importer = fast.ImageFileImporter.create(fast.Config.getTestDataPath() + 'US/JugularVein/US-2D_100.mhd')
-    segmentation = fast.BinaryThresholding.create(100).connect(importer)
+    importer = fast.ImageFileImporter(fast.Config.getTestDataPath() + 'US/JugularVein/US-2D_100.mhd')
+    segmentation = fast.BinaryThresholding(100).connect(importer)
 
     window = fast.display2D(importer, returnWindow=True)
-    renderer = fast.SegmentationRenderer.create().connect(segmentation)
+    renderer = fast.SegmentationRenderer().connect(segmentation)
     window.connect(renderer)
     if timeout:
         window.setTimeout(timeout)
@@ -56,7 +56,7 @@ def test_display2D_return_window():
 
 
 def test_display2D_mesh():
-    importer = fast.ImageFileImporter.create(fast.Config.getTestDataPath() + 'US/JugularVein/US-2D_100.mhd')
+    importer = fast.ImageFileImporter(fast.Config.getTestDataPath() + 'US/JugularVein/US-2D_100.mhd')
     image = importer.runAndGetOutputData()
     spacing = image.getSpacing()
 
@@ -70,7 +70,7 @@ def test_display2D_mesh():
         fast.MeshLine(1, 2),
     ]
 
-    mesh = fast.Mesh.create(vertices, lines)
+    mesh = fast.Mesh(vertices, lines)
 
     fast.display2D(
         importer,
@@ -97,13 +97,13 @@ def test_display2D_no_input():
 
 
 def test_display2D_image_pyramid():
-    importer = fast.WholeSlideImageImporter.create(fast.Config.getTestDataPath() + 'WSI/CMU-1.svs')
+    importer = fast.WholeSlideImageImporter(fast.Config.getTestDataPath() + 'WSI/CMU-1.svs')
     fast.display2D(imagePyramid=importer, timeout=timeout)
 
 
 def test_display2D_widget():
     widget = fast.TextWidget('Widget!')
-    importer = fast.ImageFileImporter.create(fast.Config.getTestDataPath() + 'US/JugularVein/US-2D_100.mhd')
+    importer = fast.ImageFileImporter(fast.Config.getTestDataPath() + 'US/JugularVein/US-2D_100.mhd')
     fast.display2D(image=importer, timeout=timeout, widgets=[widget])
 
 
@@ -114,7 +114,7 @@ def test_display2D_widgets():
     widget_bottom = fast.TextWidget('Bottom')
     widget_top = fast.TextWidget('Top')
 
-    importer = fast.ImageFileImporter.create(fast.Config.getTestDataPath() + 'US/JugularVein/US-2D_100.mhd')
+    importer = fast.ImageFileImporter(fast.Config.getTestDataPath() + 'US/JugularVein/US-2D_100.mhd')
 
     fast.display2D(image=importer, timeout=timeout, widgets={
         fast.WidgetPosition_RIGHT: [widget_right1, widget_right2],
@@ -125,13 +125,13 @@ def test_display2D_widgets():
 
 
 def test_display3D_slicer():
-    importer = fast.ImageFileImporter.create(fast.Config.getTestDataPath() + 'CT/CT-Thorax.mhd')
+    importer = fast.ImageFileImporter(fast.Config.getTestDataPath() + 'CT/CT-Thorax.mhd')
     fast.display3D(importer, timeout=timeout)
 
 
 def test_display3D_slicer_segmentation():
-    importer = fast.ImageFileImporter.create(fast.Config.getTestDataPath() + 'CT/CT-Thorax.mhd')
-    segmentation = fast.BinaryThresholding.create(300).connect(importer)
+    importer = fast.ImageFileImporter(fast.Config.getTestDataPath() + 'CT/CT-Thorax.mhd')
+    segmentation = fast.BinaryThresholding(300).connect(importer)
     fast.display3D(
         importer,
         segmentation,
@@ -144,7 +144,7 @@ def test_display3D_slicer_segmentation():
 
 
 def test_display3D_alpha_blending():
-    importer = fast.ImageFileImporter.create(fast.Config.getTestDataPath() + 'CT/CT-Thorax.mhd')
+    importer = fast.ImageFileImporter(fast.Config.getTestDataPath() + 'CT/CT-Thorax.mhd')
     fast.display3D(
         importer,
         displayType=fast.DisplayType.ALPHA_BLENDING,
@@ -152,7 +152,7 @@ def test_display3D_alpha_blending():
     )
 
 def test_display3D_mip():
-    importer = fast.ImageFileImporter.create(fast.Config.getTestDataPath() + 'CT/CT-Thorax.mhd')
+    importer = fast.ImageFileImporter(fast.Config.getTestDataPath() + 'CT/CT-Thorax.mhd')
     fast.display3D(
         importer,
         displayType=fast.DisplayType.MAXIMUM_INTENSITY_PROJECTION,
