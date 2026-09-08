432b24700d51d60e9f079eeedad9637fecc27cb2
Erik Smistad <ersmistad@gmail.com>
2026-09-08 11:00:44 +0200

    Debugging post processing script

--- cmake/ModulePython.cmake
+++ cmake/ModulePython.cmake
@@ -120,7 +120,7 @@ if(FAST_MODULE_Python)
         COMMAND ${CMAKE_COMMAND} -E env PYTHONPATH="${PROJECT_BINARY_DIR}/python/" ${PYTHON_EXECUTABLE} ${PROJECT_SOURCE_DIR}/source/FAST/Python/post_process_pyfast.py
         WORKING_DIRECTORY ${PROJECT_BINARY_DIR}/python/
     )
-    add_dependencies(python-post-process install_to_wheel)
+    add_dependencies(python-post-process _fast install_to_wheel)
 
     add_custom_target(python-wheel
         COMMAND ${CMAKE_COMMAND} -E copy ${PROJECT_SOURCE_DIR}/source/FAST/Python/__init__fast.py ${PROJECT_BINARY_DIR}/python/fast/__init__.py
@@ -136,7 +136,7 @@ if(FAST_MODULE_Python)
             -D OSX_ARCHITECTURE:STRING=${OSX_ARCHITECTURE}
             -P "${PROJECT_SOURCE_DIR}/cmake/PythonWheel.cmake")
 
-    add_dependencies(python-wheel install_to_wheel python-post-process)
+    add_dependencies(python-wheel python-post-process)
 else()
     message("-- Python module not enabled in CMake, Python bindings will NOT be created.")
 endif()
